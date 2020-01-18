470961a9cb821e9a5bd9f10d10dd06fbbbfd7d06
akash.rajput <akash.rajput@causecode.com>
2020-01-19 02:43:27 +0530

    Made stuff work

--- publish-alpha.sh
+++ publish-alpha.sh
@@ -2,5 +2,5 @@
 
 circleci config pack src >orb.yml
 circleci orb validate orb.yml
-circleci orb publish orb.yml snippet-software/aws-deploy@dev:alpha-1
+circleci orb publish orb.yml snippet-software/aws-deploy@dev:alpha-7
 rm -rf orb.yml
