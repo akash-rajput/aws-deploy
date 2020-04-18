a2f0710cec66b84902a6704538c16e40f3b3a457
akash-rajput <devakashrajput@gmail.com>
2020-04-18 20:00:21 +0530

    production deployment

--- publish-alpha.sh
+++ publish-alpha.sh
@@ -2,5 +2,5 @@
 
 circleci config pack src >orb.yml
 circleci orb validate orb.yml
-circleci orb publish orb.yml snippet-software/aws-deploy@dev:alpha-7
+circleci orb publish orb.yml snippet-software/aws-deploy@0.0.1
 rm -rf orb.yml
